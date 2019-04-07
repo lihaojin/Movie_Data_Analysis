55cf6c54461e0f47202c319dd024b8e9f829b5da
khelif96 <khelif96@gmail.com>
2019-04-06 21:45:56 -0400

    Moved download to main jupyter notebook file

--- downloadData.py
+++ downloadData.py
@@ -9,17 +9,20 @@ import subprocess
 import os.path
 from dotenv import load_dotenv
 load_dotenv()
-zipPath = '/content/the-movies-dataset.zip' if imUsingColab else 'the-movies-dataset.zip'
-zipExists = os.path.isfile(zipPath)
 
-if not zipExists:
-    print(subprocess.check_output(['kaggle', 'datasets', 'download', '-d', 'rounakbanik/the-movies-dataset']))
+def download(imUsingColab=False):
+    zipPath = '/content/the-movies-dataset.zip' if imUsingColab else 'the-movies-dataset.zip'
+    zipExists = os.path.isfile(zipPath)
 
-print("Opening zip file")
-zip_ref = zipfile.ZipFile(zipPath, 'r')
-print(zip_ref.namelist())
-print("Loaded Zip File")
-print("Attempting extraction")
-zip_ref.extractall('./data')
-print("Extraction Completed")
-zip_ref.close()
+    if not zipExists:
+        print("Couldn't Find dataset downloading now")
+        print(subprocess.check_output(['kaggle', 'datasets', 'download', '-d', 'rounakbanik/the-movies-dataset']))
+
+    print("Opening zip file")
+    zip_ref = zipfile.ZipFile(zipPath, 'r')
+    print(zip_ref.namelist())
+    print("Loaded Zip File")
+    print("Attempting extraction")
+    zip_ref.extractall('./data')
+    print("Extraction Completed")
+    zip_ref.close()
