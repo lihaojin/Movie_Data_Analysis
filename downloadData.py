4e8887bc85150b452a426f58ed8722183f9a9e34
khelif96 <khelif96@gmail.com>
2019-04-06 20:56:29 -0400

    Dataset download script

---
+++ downloadData.py
@@ -0,0 +1,25 @@
+imUsingColab = False
+#
+# if imUsingColab:
+#     !pip install python-dotenv
+#     !pip install kaggle
+
+import zipfile
+import subprocess
+import os.path
+from dotenv import load_dotenv
+load_dotenv()
+zipPath = '/content/the-movies-dataset.zip' if imUsingColab else 'the-movies-dataset.zip'
+zipExists = os.path.isfile(zipPath)
+
+if not zipExists:
+    print(subprocess.check_output(['kaggle', 'datasets', 'download', '-d', 'rounakbanik/the-movies-dataset']))
+
+print("Opening zip file")
+zip_ref = zipfile.ZipFile(zipPath, 'r')
+print(zip_ref.namelist())
+print("Loaded Zip File")
+print("Attempting extraction")
+zip_ref.extractall('./data')
+print("Extraction Completed")
+zip_ref.close()
